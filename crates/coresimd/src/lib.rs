f136a842e3a998bc42582f926ba7de2b2ace3e98
Peter Jin <peterhaijin@gmail.com>
2018-12-28 11:17:15 -0500

    Debugging the integer_atomics problem.

--- crates/coresimd/src/lib.rs
+++ crates/coresimd/src/lib.rs
@@ -41,10 +41,11 @@
 // a compile-time error: 'unknown feature `integer_atomics`'. This ought to be
 // investigated further, but for now just disable "integer_atomics" so we can
 // run _some_ test for the nvptx/nvptx64 targets.
-#![cfg_attr(
+/*#![cfg_attr(
     not(any(target_arch = "nvptx", target_arch = "nvptx64")),
     feature(integer_atomics)
-)]
+)]*/
+#![feature(integer_atomics)]
 #![cfg_attr(test, feature(test, abi_vectorcall, untagged_unions))]
 #![cfg_attr(
     feature = "cargo-clippy",
