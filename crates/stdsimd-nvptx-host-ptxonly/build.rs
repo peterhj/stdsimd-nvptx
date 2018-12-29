0a50a03dfbd621bed5d16d9637f9c2838c2e182e
Peter Jin <peterhaijin@gmail.com>
2018-12-28 16:20:23 -0800

    Write the generated testglue in the output directory.

--- crates/stdsimd-nvptx-host-ptxonly/build.rs
+++ crates/stdsimd-nvptx-host-ptxonly/build.rs
@@ -2,7 +2,9 @@ extern crate nvptxglue;
 
 use nvptxglue::prelude::*;
 
+use std::env;
 use std::io::{Write, Result as IoResult};
+use std::path::{PathBuf};
 
 struct PtxOnlyTestGlue;
 
@@ -12,24 +14,26 @@ impl Glue for PtxOnlyTestGlue {
             &CompilerOutput::Ptx(ref path) => path.to_str().unwrap(),
             _ => panic!(),
         };
-        writeln!(writer, "static _REF_PTX: &'static str = include_str!(\"../assets/reference.ptx\");")?;
+        writeln!(writer, "use crate::{{REF_PTX}};")?;
+        writeln!(writer, "")?;
         writeln!(writer, "static _TEST_PTX: &'static str = include_str!(\"{}\");", ptx_path)?;
         writeln!(writer, "")?;
         writeln!(writer, "#[test]")?;
         writeln!(writer, "fn test_ptx_equal_to_reference_ptx() {{")?;
-        writeln!(writer, "    assert_eq!(_TEST_PTX, _REF_PTX);")?;
+        writeln!(writer, "    assert_eq!(_TEST_PTX, REF_PTX);")?;
         writeln!(writer, "}}")?;
         Ok(())
     }
 }
 
 fn main() {
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     nvptxglue::Builder::default()
         .crate_dir("../stdsimd-nvptx-kernels")
         .gencode(Gencode::Ptx(Cc_3_5))
         .whitelist_kernel("_stdsimd_nvptx_test_syncthreads_kernel")
         .compile(Phase::Ptx)
         .expect("nvptxglue failure: compile to ptx")
-        .write_bindings_to_file(PtxOnlyTestGlue, "src/tests.rs")
+        .write_bindings_to_file(PtxOnlyTestGlue, out_dir.join("testglue.rs"))
         .expect("nvptxglue failure: generate testglue bindings");
 }
--- crates/stdsimd-nvptx-host-ptxonly/src/lib.rs
+++ crates/stdsimd-nvptx-host-ptxonly/src/lib.rs
@@ -1,2 +1,6 @@
+pub static REF_PTX: &'static str = include_str!("../assets/reference.ptx");
+
 #[cfg(test)]
-mod tests;
+mod tests {
+include!(concat!(env!("OUT_DIR"), "/testglue.rs"));
+}
